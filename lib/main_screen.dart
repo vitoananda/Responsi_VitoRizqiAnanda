754d26b7966427bd8638a16f0aaa3371833db367
vitoananda <vtoananda@gmail.com>
2022-12-06 16:25:17 +0700

    Responsi Vito Ananda (124200044)

---
+++ lib/main_screen.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:responsi_124200044/data_source.dart';
+import 'package:responsi_124200044/detail_page.dart';
+import 'package:responsi_124200044/matches_model.dart';
+import 'detail_matches_model.dart';
+
+
+class Menu extends StatefulWidget {
+  const Menu({Key? key}) : super(key: key);
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8),
+          child: Image.network(
+            "https://static.vecteezy.com/system/resources/previews/008/785/472/original/fifa-world-cup-qatar-2022-official-logo-mondial-champion-symbol-design-abstract-illustration-free-vector.jpg",
+          ),
+        ),
+        title: Text(
+          "World Cup Qatar 2022",
+          style:
+              TextStyle(color: Color(0xff0800000), fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+        child: FutureBuilder(
+            future: MatchesSource.instance.LoadMatches(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.hasError) {
+                return _buildErrorSection();
+              }
+              if (snapshot.hasData) {
+                // MatchesModel1 matchesModel1 = MatchesModel1.fromJson(snapshot.data);
+                return _buildSuccessSection(snapshot.data);
+              }
+              return _buildLoadingSection();
+            }),
+      ),
+    );
+  }
+
+  Widget _buildErrorSection() {
+    return Text("Error");
+  }
+
+  Widget _buildLoadingSection() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  // Widget _buildSuccessSection(MatchesModel data){
+  Widget _buildSuccessSection(List<dynamic> data) {
+    return ListView.builder(
+      itemCount: 44,
+      itemBuilder: (BuildContext context, int index) {
+        MatchesModel matchesModel = MatchesModel.fromJson(data[index]);
+        DetailMatchesModel detailMatchesModel =
+            DetailMatchesModel.fromJson(data[index]);
+        return InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MatchDetail(
+                  detail: matchesModel,
+                ),
+              ),
+            );
+          },
+          child: Container(
+            height: 100,
+            width: 200,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12), // if you need this
+                side: BorderSide(
+                  color: Colors.black.withOpacity(0.1),
+                  width: 3,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.25),
+                            spreadRadius: 3,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      width: 70,
+                      height: 50,
+                      child: FittedBox(
+                          fit: BoxFit.fill,
+                          child: Image.network(
+                              'https://countryflagsapi.com/png/${matchesModel!.homeTeam!.name!}'))),
+                  Text(("${matchesModel!.homeTeam!.name!}")),
+                  Text(("${matchesModel!.homeTeam!.goals!} - ${matchesModel!.awayTeam!.goals!}")),
+                  Text(("${matchesModel!.awayTeam!.name!}")),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.25),
+                          spreadRadius: 3,
+                          blurRadius: 5,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    width: 70,
+                    height: 50,
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.network(
+                          'https://countryflagsapi.com/png/${matchesModel!.awayTeam!.name!}'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+      //
+    );
+  }
+}
